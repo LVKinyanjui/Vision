02379d3254a0bdfbb306d12c6023a90dd615c96b
LVKinyanjui <leslievictorius@gmail.com>
2024-08-27 17:28:42 +0300

    Ran first functional ocr app

--- Tesseract/app.py
+++ Tesseract/app.py
@@ -1,3 +1,22 @@
 import streamlit as st
+import pytesseract
+from PIL import Image
 
-st.write("## Tesseract OCR")
+
+st.write("## Tesseract OCR \n A simple Image to Text Application")
+
+#TODO: Limit input to supported image files
+file = st.file_uploader("Choose a file", type=['png', 'jpg'])
+
+@st.cache_data
+def transcribe(file):
+    image = Image.open(file)
+    return pytesseract.image_to_string(image)
+
+if file is not None:
+    st.write("Image uploaded succesfully!")
+
+    st.write("### Recognized Text")
+    ocr_text = transcribe(file)
+    ocr_text
+
